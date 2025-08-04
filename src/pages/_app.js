3f6412f6245eef348bf625bd2534d34bb090a258
Kyle Fernandez <fkyleowie@gmail.com>
2025-08-04 21:40:27 +0800

    Homepage done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import "@/styles/globals.css";
+import theme from '../theme';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ColorModeScript initialColorMode="dark" />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
